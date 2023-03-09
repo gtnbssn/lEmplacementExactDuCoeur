4498ebef2499e0a916633d0531784be4e8c9e7cc
gtnbssn <gaetan.boisson@gmail.com>
2023-03-09 21:53:08 +0800

    make things more async

--- src/lib/components/PeerSetup.svelte
+++ src/lib/components/PeerSetup.svelte
@@ -1,44 +1,33 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import Peer from 'peerjs';
 	import VisibilityChange from 'svelte-visibility-change';
 	import { connectionStateStore, sendMessageToPeers } from '$lib/stores';
 
 	let visibilityState: 'visible' | 'hidden';
 
-	const peer = new Peer();
+	let peer: Peer;
 
-	peer.on('open', (id) => {
-		$connectionStateStore.peerid = id;
-		handleConnect(id);
-		$connectionStateStore.firstLoad = false;
-	});
+	// peer.on('open', (id) => {
+	// 	$connectionStateStore.peerid = id;
+	// 	handleConnect(id);
+	// 	$connectionStateStore.firstLoad = false;
+	// });
 
-	peer.on('connection', (dataConnection) => {
-		dataConnection.on('open', () => {
-			console.log(`connection from ${dataConnection.peer}`);
-			$connectionStateStore.peerConnections = [
-				...$connectionStateStore.peerConnections,
-				dataConnection
-			];
-			dataConnection.on('data', (data) => {
-				$connectionStateStore.messagesHistory = [
-					...$connectionStateStore.messagesHistory,
-					String(data)
-				];
-				console.log(data);
+	const getPeerID = async (): Promise<string> => {
+		if (peer.id !== undefined) return Promise.resolve(peer.id);
+		return new Promise((resolve) => {
+			peer.on('open', (id) => {
+				console.log('openin da connection');
+				resolve(id);
 			});
 		});
-		dataConnection.on('close', () => {
-			console.log(`${dataConnection.peer} has closed the connection`);
-			$connectionStateStore.peerConnections = $connectionStateStore.peerConnections.filter(
-				(peerConnection) => peerConnection.peer != dataConnection.peer
-			);
-		});
-	});
+	};
 
 	const connectPeers = (peers: string[]) => {
 		peers.forEach((peerid) => {
 			const dataConnection = peer.connect(peerid);
+			console.log(`trying to connect to ${dataConnection.peer}`);
 			dataConnection.on('open', () => {
 				console.log(`connecting to ${dataConnection.peer}`);
 				$connectionStateStore.peerConnections = [
@@ -56,14 +45,54 @@
 		});
 	};
 
-	const handleConnect = async (id: string) => {
-		const res = await fetch(`/api/connect?peerid=${id}`);
-		const json = await res.json();
-		console.log('connecting');
-		console.log(`json sent by the connect endpoint:`);
-		console.log(json);
-		$connectionStateStore.userid = json.currentUserId;
-		connectPeers(json.peers as string[]);
+	const handleConnect = async () => {
+		// peer = new Peer({
+		// 	config: {
+		// 		iceServers: [
+		// 			{ url: 'stun:coturn.gtnbssn.com:443' },
+		// 			{ url: 'turn:coturn.gtnbssn.com:443', username: 'gtnbssn', credential: 'hatchinmarch' }
+		// 		]
+		// 	}
+		// });
+		peer = new Peer();
+		peer.on('open', (id) => {
+			$connectionStateStore.peerid = id;
+			fetch(`/api/connect?peerid=${$connectionStateStore.peerid}`)
+				.then((res) => res.json())
+				.then((json) => {
+					console.log(`peerid is ${$connectionStateStore.peerid}`);
+					console.log('connecting');
+					console.log(`json sent by the connect endpoint:`);
+					console.log(json);
+					$connectionStateStore.userid = json.currentUserId;
+					connectPeers(json.peers as string[]);
+				});
+			$connectionStateStore.firstLoad = false;
+		});
+		peer.on('connection', (dataConnection) => {
+			dataConnection.on('open', () => {
+				console.log(`connection from ${dataConnection.peer}`);
+				$connectionStateStore.peerConnections = [
+					...$connectionStateStore.peerConnections,
+					dataConnection
+				];
+				dataConnection.on('data', (data) => {
+					$connectionStateStore.messagesHistory = [
+						...$connectionStateStore.messagesHistory,
+						String(data)
+					];
+					console.log(data);
+				});
+			});
+			dataConnection.on('close', () => {
+				console.log(`${dataConnection.peer} has closed the connection`);
+				$connectionStateStore.peerConnections = $connectionStateStore.peerConnections.filter(
+					(peerConnection) => peerConnection.peer != dataConnection.peer
+				);
+			});
+		});
+		// const peerid = await getPeerID();
+		// $connectionStateStore.peerid = $connectionStateStore.peerid;
 	};
 
 	const handleReconnect = async () => {
@@ -81,26 +110,28 @@
 		if ($connectionStateStore.firstLoad) {
 			return;
 		}
-		await fetch(`/api/disconnect?userid=${$connectionStateStore.userid}`);
 		$connectionStateStore.peerConnections.forEach((peerConnection) => peerConnection.close());
 		$connectionStateStore.peerConnections = [];
+		await fetch(`/api/disconnect?userid=${$connectionStateStore.userid}`);
 		console.log('disconnecting');
 		console.log($connectionStateStore);
 		return 'disconnected';
 	};
 
-  const delays = [500, 1000, 1500, 2000, 2500, 3000];
-  const messages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	onMount(async () => handleConnect());
+
+	const delays = [500, 1000, 1500, 2000, 2500, 3000];
+	const messages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 	const sendMessage = () => {
-    const randomDelay = delays[Math.floor(Math.random() * delays.length)];
+		const randomDelay = delays[Math.floor(Math.random() * delays.length)];
 		console.log(`next message in ${randomDelay} ms.`);
-    if($connectionStateStore.peerConnections.length === 0) {
-      console.log("no connected peers");
-    }else{
-      const randomMessage = String(messages[Math.floor(Math.random() * messages.length)]);
-      console.log(`sending ${randomMessage}`);
-      sendMessageToPeers(randomMessage, $connectionStateStore.peerConnections);
-    }
+		if ($connectionStateStore.peerConnections.length === 0) {
+			console.log('no connected peers');
+		} else {
+			const randomMessage = String(messages[Math.floor(Math.random() * messages.length)]);
+			console.log(`sending ${randomMessage}`);
+			sendMessageToPeers(randomMessage, $connectionStateStore.peerConnections);
+		}
 		setTimeout(sendMessage, randomDelay);
 	};
 	setTimeout(sendMessage, 1000);
@@ -110,6 +141,6 @@
 
 <VisibilityChange
 	bind:state={visibilityState}
-	on:hidden={() => handleDisconnect()}
-	on:visible={() => handleReconnect()}
+	on:hidden={async () => await handleDisconnect()}
+	on:visible={async () => await handleReconnect()}
 />
