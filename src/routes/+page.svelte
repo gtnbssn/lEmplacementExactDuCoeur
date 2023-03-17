f35f825f541378ebce4afef544fb423006add59c
gtnbssn <gaetan.boisson@gmail.com>
2023-03-17 14:59:23 +0800

    remove softiness in spring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,17 +7,14 @@
 
 	const handleMotion = (evt: DeviceMotionEvent) => {
 		if (evt.acceleration) {
-			deviceAcceleration.set(
-				{
-					x: evt.acceleration.x || 0,
-					y: evt.acceleration.y || 0,
-					z: evt.acceleration.z || 0,
-					alpha: evt.rotationRate?.alpha || 0,
-					beta: evt.rotationRate?.beta || 0,
-					gamma: evt.rotationRate?.gamma || 0
-				},
-				{ soft: 0.5 }
-			);
+			deviceAcceleration.set({
+				x: evt.acceleration.x || 0,
+				y: evt.acceleration.y || 0,
+				z: evt.acceleration.z || 0,
+				alpha: evt.rotationRate?.alpha || 0,
+				beta: evt.rotationRate?.beta || 0,
+				gamma: evt.rotationRate?.gamma || 0
+			});
 		}
 	};
 	const startMotionEvents = () => {
